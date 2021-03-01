83f594f4f6c13bf1823095422512376143f029f8
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-02-28 17:13:06 -0700

    Finished and ready to be graded.

--- imagequiz/src/components/Login.js
+++ imagequiz/src/components/Login.js
@@ -1,9 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+
 function Login(){
     return (
-        <div>
+        <div class="text-center">
             <h2>Login</h2>
 
-            
+            <Form class="w-75 border border-dark rounded p-4 mx-auto">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Link to="/home">
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Link>
+            </Form>
         </div>
     );
 }
